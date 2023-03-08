0b625759dd210e9ea62fc91b1311ebee96c515df
Tuval Simha <tuval.simha@Gmail.com>
2023-03-08 09:47:04 +0200

    fix property (#13)
    Signed-off-by: Tuval Simha <tuval.simha@gmail.com>

--- src/models/property-class.js
+++ src/models/property-class.js
@@ -1,6 +1,15 @@
 class Property {
-  constructor({ id, name, price, description, imgUrl }) {
-    Object.assign(this, { id, name, price, description, imgUrl });
+  id;
+  name;
+  price;
+  description;
+  imgUrl;
+  constructor(id, name, price, description, imgUrl) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.imgUrl = imgUrl;
   }
 }
 export default Property;
